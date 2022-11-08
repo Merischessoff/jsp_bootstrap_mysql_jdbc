af1524fd3a6a07e3726ef522ca392dd25921ec64
Merischessoff <gonkaschessoff@gmail.com>
2022-11-07 22:07:28 -0300

    atualizando sistema

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -7,7 +7,7 @@ public class ConnectionFactory {
     public Connection getConnection() throws ClassNotFoundException, SQLException{
            Class.forName("com.mysql.jdbc.Driver");
            String urlBD="jdbc:mysql://localhost:3306/bd";
-           return DriverManager.getConnection(urlBD, "root", "teste123");
+           return DriverManager.getConnection(urlBD, "root", "!12@jac#");
     }
 }
 
